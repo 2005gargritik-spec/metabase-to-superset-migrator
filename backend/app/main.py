7ee42e0adbffa95b1e31a597e903820bcbea2e43
Ritik's Macbook <apple@Ritiks-Macbook-Pro.local>
2026-07-09 15:39:55 +0100

    Enable Vercel CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://metabase-to-superset-migrator.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
